945612be39a8fd632020e2df870c0f1c71efc825
Dennis Angeline <dangeline@live.com>
2014-08-13 22:57:03 -0700

    changed stopwatch

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,21 +12,54 @@ namespace Quicksort
 	{
 		static void Main(string[] args)
 		{
-			int CompCount = 0;
-			string filename = "QuickSort.txt";
+			SortRandoms();
+			SortFromFile();
 
-			Console.WriteLine(string.Format("Reading input array from file [{0}]", filename));
-			int[] unsorted = GetArrayFromFile(filename);
-
-			// Sort the array
-			Quicksort(unsorted, 0, unsorted.Length - 1, ref CompCount);
-
-			Console.WriteLine(string.Format("Array sorted with [{0}] comparisons", CompCount));
-
-			Console.WriteLine("Press any key to continue...");
-
-			Console.ReadLine();
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
 		}
+
+		private static void SortFromFile()
+		{
+			string filename = "QuickSort.txt";
+			Console.WriteLine(string.Format("Reading input array from file [{0}]", filename));
+
+			int CompCount = 0;
+
+			int[] ArrayToSort = GetArrayFromFile(filename);
+
+			// Time MergeSort using watch1
+			Stopwatch watch1 = new Stopwatch();
+			Console.WriteLine("Beginning Sort");
+
+			watch1.Start();
+			Quicksort(ArrayToSort, 0, ArrayToSort.Length - 1, ref CompCount);
+			watch1.Stop();
+
+			Console.WriteLine(String.Format("Sort complete in {0:f3} sec with [{1:n0}] comparisons", watch1.ElapsedMilliseconds/1000.0, CompCount));
+		}
+
+
+		private static void SortRandoms()
+		{
+			// Initialize an array of size ElemCnt;
+			int CompCount = 0;
+			const int ElemCnt = 5000000;
+			const int ElemMax = 1000000;
+
+			int[] ArrayToSort = GetRandomArray(ElemCnt, ElemMax);
+
+			// Time MergeSort using watch1
+			Stopwatch watch1 = new Stopwatch();
+			Console.WriteLine("Beginning Sort");
+
+			watch1.Start();
+			Quicksort(ArrayToSort, 0, ArrayToSort.Length - 1, ref CompCount);
+			watch1.Stop();
+
+			Console.WriteLine(String.Format("Sort complete in {0:f3} sec with [{1:n0}] comparisons", watch1.ElapsedMilliseconds/1000.0, CompCount));
+		}
+
 
 		public static int Quicksort(int[] elements, int left, int right, ref int CompCount)
 		{
@@ -95,6 +129,19 @@ namespace Quicksort
 				return c;
 		}
 
+		private static int[] GetRandomArray(int count, int max)
+		{
+			int[] RandomArray = new int[count];
+
+			// Initialize the element to random numbers
+			Console.WriteLine(String.Format("Initializing array with {0:n0} random values", count));
+			Random RGen = new Random(DateTime.Now.Millisecond);
+			for (int i = 0; i < count; i++)
+				RandomArray[i] = RGen.Next(max);
+
+			return RandomArray;
+		}
+		
 		private static int[] GetArrayFromFile(string filename)
 		{
 			StreamReader file = new StreamReader(filename);
